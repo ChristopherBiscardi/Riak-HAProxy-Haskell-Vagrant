6783f2bd17bc4c8b9663448280ba2d3d459d9271
Christopher Biscardi <biscarch@sketcht.com>
2014-02-15 16:57:33 -0800

    Save Basho some bandwidth by making the pre11 .deb package local.

--- vagrant-files/riak-build.sh
+++ vagrant-files/riak-build.sh
@@ -6,8 +6,9 @@ echo $2
 echo $1
 sudo apt-get update
 sudo apt-get install libssl0.9.8 default-jre -y
-wget http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.0pre11/ubuntu/precise/riak_2.0.0pre11-1_amd64.deb
-sudo dpkg -i riak_2.0.0pre11-1_amd64.deb
+#wget http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.0pre11/ubuntu/precise/riak_2.0.0pre11-1_amd64.deb
+#sudo dpkg -i riak_2.0.0pre11-1_amd64.deb
+dpkg -i /vagrant/vagrant-files/deps/riak_2.0.0pre11-1_amd64.deb
 sed -i "s/127.0.0.1/$1/g" /etc/riak/riak.conf
 sed -i 's/search = off/search = on/g' /etc/riak/riak.conf
 ulimit -n 8192
